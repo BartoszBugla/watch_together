fa4e335512fcd3bccb3ad989b1f9f254407f7e0a
Bartosz Bugla <bartoszbugla@gmail.com>
2021-04-19 22:38:10 +0200

    sdas

--- pages/api/rooms/index.ts
+++ pages/api/rooms/index.ts
@@ -1,11 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-import { v4 as uuidv4 } from "uuid";
+
 import ApiHandler from "helpers/apiHandler";
 const prisma = new PrismaClient();
 const handler = async (req, res) => {
-  const uuid = uuidv4();
-
   await ApiHandler(req, res, "POST", async () => {
     const newRoom = await prisma.room.create({
       data: {
